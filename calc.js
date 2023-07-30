7ac66ace2e02185a07f39e88772601e713128379
Akpose <122671706+Akpose@users.noreply.github.com>
2023-07-30 20:56:43 +0100

    Add delete button and more styles to the UI

--- calc.js
+++ calc.js
@@ -1,29 +1,35 @@
 
-let num1;
-let num2;
-let operator;
+// let num1;
+// let num2;
+// let operator;
 
-const operations = {
-    "+" : function add(num1, num2) {
-        return num1 + num2;
-    },
-    "-" : function subtract(num1, num2) {
-        return num1 - num2;
-    },
-    "/" : function divide(num1, num2) {
-        return num1 / num2;
-    },
-    "*" : function multuply(num1, num2) {
-        return num1 * num2;
-    },
-}
+// const operations = {
+//     "+" : function add(num1, num2) {
+//         return num1 + num2;
+//     },
+//     "-" : function subtract(num1, num2) {
+//         return num1 - num2;
+//     },
+//     "/" : function divide(num1, num2) {
+//         return num1 / num2;
+//     },
+//     "*" : function multuply(num1, num2) {
+//         return num1 * num2;
+//     },
+// }
 
 
-function operate(list, operator) {
-   return list.reduce(operations[operator]);
-}
+// function operate(list, operator) {
+//    return list.reduce(operations[operator]);
+// }
 
-console.log(operate([4,2], "+"));
+// console.log(operate([4,2], "+"));
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => button.addEventListener('click', function (e) {
+    console.log(e.target.value);
+}))
+
 
 
 
